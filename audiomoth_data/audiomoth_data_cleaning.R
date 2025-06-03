b6a1ece6aab1371788faf2d89a824a2a8e22c415
c-noonan-02 <charlottenoonan@outlook.com>
2025-06-03 17:25:40 +0100

    Adapted code to extract from the filepath stored in the dataframe instead.

--- audiomoth_data/audiomoth_data_cleaning.R
+++ audiomoth_data/audiomoth_data_cleaning.R
@@ -2,32 +2,37 @@
 rm(list=ls())
 
 # load required packages
-library(readxl)
-library(writexl)
 library(dplyr)
+library(stringr)
+library(tools)
 
-# add meta data to a single excel file
-file_path <- "F:/dissertation_data/pilot_woodland/BirdNET_Output/Audiomoth_1/20250515_000000.BirdNET.results.csv"
+# read in the example file
+data <- read.csv(file_path)
+head(data) # check data
 
-# extract sections of the file path
-path_parts <- strsplit(file_path, "/|\\\\")[[1]] # this splits on \ or /
-path_parts # check
+# use a loop to extract meta data from the file path stored in each row of the data frame
+data <- data %>% 
+  mutate(
+    
+    # extract and save the audiomoth ID
+    audiomoth_ID = sapply(strsplit(File, "/|\\\\"), function(x) x[length(x) - 1]), 
+    
+    # extract the habitat/site info
+    site_ID = sapply(strsplit(File, "/|\\\\"), function(x) x[length(x) - 2]), # will give a better folder name eventually
+    
+    # extract the file name
+    file_name = file_path_sans_ext(basename(File)),
+    
+    # extract the recording date
+    recording_date = as.Date(substr(file_name, 1, 8), format = "%Y%m%d"), 
+    
+    # extract the start time of the recording
+    recording_time = ifelse(
+      nchar(file_name) >= 15,
+      format(strptime(substr(file_name, 10, 15), format = "%H%M%S"), "%H%M%S"),
+      NA
+    )
+  ) %>%
+  select(-file_name) # do not save path_parts or file_name as new columns
 
-# extract audiomoth_ID
-audiomoth_ID <- path_parts[length(path_parts) - 1]
-audiomoth_ID # check
-
-# extract the filename
-file_name <- tools::file_path_sans_ext(basename(file_path))
-file_name # check
-# remove ".BirdNET.results"
-
-# extract the date
-date_str <- substr(file_name, 1, 8)
-recording_date <- as.Date(date_str, format = "%Y%m%d")
-recording_date # check
-
-# extract the time
-time_str <- substr(file_name, 10, 15)
-recording_time <- format(strptime(time_str, format = "%H%M%S"), "%H%M%S")
-recording_time # check
+head(data)
