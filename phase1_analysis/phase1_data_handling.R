6ededdf0967b2854d9f61ccfb6b4646a00972cf5
c-noonan-02 <charlottenoonan@outlook.com>
2025-06-04 17:27:33 +0100

    Adapted code to get summary of presence absence data from different recording periods and sampling schedules.

--- phase1_analysis/phase1_data_handling.R
+++ phase1_analysis/phase1_data_handling.R
@@ -10,6 +10,7 @@ library(tidyr)
 library(readxl)
 library(writexl)
 # library(tidyverse)
+library(hms)
 
 # import data set
 BD_pilot_data <- read_xlsx("./audiomoth_data/BD2025_BirdNETOutput2.xlsx") # times preserved in xlsx format
@@ -146,10 +147,10 @@ head(BD_pilot_threeday)
 # but doing by each division for now
 
 # list of all devices and all species
-sites <- unique(BD_pilot_days$site)
-audiomoths <- unique(BD_pilot_days$audiomoth_ID)
-species_c <- unique(BD_pilot_days$common_n)
-species_s <- unique(BD_pilot_days$scientific_n)
+sites <- unique(BD_pilot_data$site)
+audiomoths <- unique(BD_pilot_data$audiomoth_ID)
+species_c <- unique(BD_pilot_data$common_n)
+species_s <- unique(BD_pilot_data$scientific_n)
 
 # create grid of all combinations
 full_grid <- expand.grid(site = sites, audiomoth_ID = audiomoths, common_n = species_c, scientific_n = species_s)
@@ -209,11 +210,92 @@ View(BD_pilot_days_ab)
 
 BD_pilot_period <- BD_pilot_data
 
+# list all detection times
+unique(BD_pilot_period$detect_start_time)
+
+
 ##### Dawn-only (7:00-9:00) #####
+
+# add logical column in main data frame to highlight rows which were recorded within a dawn survey
+BD_pilot_period <- BD_pilot_period %>% 
+  mutate(dawn = detect_start_time >= "07:00:00" & detect_start_time < "09:00:00")
+
+# check dataset
+View(BD_pilot_period)
+unique(BD_pilot_period$dawn)
 
 ##### Dusk-only (20:00 - 22:00) #####
 
+# add logical column in main data frame to highlight rows which were recorded within a dusk survey
+BD_pilot_period <- BD_pilot_period %>% 
+  mutate(dusk = detect_start_time >= "20:00:00" & detect_start_time < "22:00:00")
+
+# check dataset
+View(BD_pilot_period)
+unique(BD_pilot_period$dusk)
+
 ##### All day (00:00 - 24:00) #####
+
+# add logical column in main data frame to highlight rows which were recorded within the full 24hrs
+BD_pilot_period$all_day <- TRUE
+
+# check dataset
+View(BD_pilot_period)
+unique(BD_pilot_period$all_day)
+
+##### Presence/Absence Summary #####
+# this might be better at the end, with all sampling designs in one for ease
+# but doing by each division for now
+
+# summarise presence/absences
+BD_pilot_period_pa <- BD_pilot_period %>% 
+  group_by(site, audiomoth_ID, common_n, scientific_n) %>% 
+  summarise(
+    dawn_sample = as.integer((any(dawn))),
+    dusk_sample = as.integer((any(dusk))),
+    day_sample = as.integer((any(all_day))),
+    .groups = "drop"
+  )
+
+# merge with the full grid to record 'missing' species as absences
+BD_pilot_period_pa <- full_grid %>% 
+  left_join(BD_pilot_period_pa, by = c("site", "audiomoth_ID", "common_n", "scientific_n")) %>% 
+  mutate(
+    dawn_sample = replace_na(dawn_sample, 0),
+    dusk_sample = replace_na(dusk_sample, 0),
+    day_sample = replace_na(day_sample, 0)
+  )
+
+# check dataframe
+View(BD_pilot_days_pa)
+
+
+
+##### No. Detections Summary #####
+
+# summarise detection counts
+BD_pilot_period_ab <- BD_pilot_period %>% 
+  group_by(site, audiomoth_ID, common_n, scientific_n) %>% 
+  summarise(
+    dusk_sample = sum(dusk),
+    dawn_sample = sum(dawn),
+    day_sample = sum(all_day),
+    .groups = "drop"
+  )
+
+# merge with the full grid to record 'missing' species as absences
+BD_pilot_period_ab <- full_grid %>% 
+  left_join(BD_pilot_period_ab, by = c("site", "audiomoth_ID", "common_n", "scientific_n")) %>% 
+  mutate(
+    dawn_sample = replace_na(dawn_sample, 0),
+    dusk_sample = replace_na(dusk_sample, 0),
+    day_sample = replace_na(day_sample, 0)
+  )
+
+# check dataframe
+View(BD_pilot_period_ab)
+
+
 
 
 #### Sub-samples to assess sampling schedule ####
@@ -222,10 +304,111 @@ BD_pilot_sched <- BD_pilot_data
 
 ##### 5mins/hour #####
 
+# add logical column in main data frame to highlight rows which were recorded in the first 5 mins of every hour
+BD_pilot_sched <- BD_pilot_sched %>% 
+  mutate(five_mins = lubridate::minute(hms::as_hms(detect_start_time)) < 5)
+
+# check dataset
+View(BD_pilot_sched)
+unique(BD_pilot_sched$five_mins)
+
 ##### 10mins/hour #####
+
+# add logical column in main data frame to highlight rows which were recorded in the first 10 mins of every hour
+BD_pilot_sched <- BD_pilot_sched %>% 
+  mutate(ten_mins = lubridate::minute(hms::as_hms(detect_start_time)) < 10)
+
+# check dataset
+View(BD_pilot_sched)
+unique(BD_pilot_sched$ten_mins)
 
 ##### 15mins/hour #####
 
+# add logical column in main data frame to highlight rows which were recorded in the first 15 mins of every hour
+BD_pilot_sched <- BD_pilot_sched %>% 
+  mutate(fithtn_mins = lubridate::minute(hms::as_hms(detect_start_time)) < 15)
+
+# check dataset
+View(BD_pilot_sched)
+unique(BD_pilot_sched$fithtn_mins)
+
 ##### 30mins/hour #####
 
+# add logical column in main data frame to highlight rows which were recorded in the first 30 mins of every hour
+BD_pilot_sched <- BD_pilot_sched %>% 
+  mutate(thirty_mins = lubridate::minute(hms::as_hms(detect_start_time)) < 30)
+
+# check dataset
+View(BD_pilot_sched)
+unique(BD_pilot_sched$thirty_mins)
+
 ##### 60mins/hour #####
+
+# add logical column in main data frame to highlight rows which were recorded using continuous recording (full hour)
+
+BD_pilot_sched$full_hour <- TRUE
+
+# check dataset
+View(BD_pilot_sched)
+unique(BD_pilot_sched$full_hour)
+
+
+##### Presence/Absence Summary #####
+# this might be better at the end, with all sampling designs in one for ease
+# but doing by each division for now
+
+# summarise presence/absences
+BD_pilot_sched_pa <- BD_pilot_sched %>% 
+  group_by(site, audiomoth_ID, common_n, scientific_n) %>% 
+  summarise(
+    five_mins = as.integer((any(five_mins))),
+    ten_mins = as.integer((any(ten_mins))),
+    fithtn_mins = as.integer((any(fithtn_mins))),
+    thirty_mins = as.integer((any(thirty_mins))),
+    full_hour = as.integer((any(full_hour))),
+    .groups = "drop"
+  )
+
+# merge with the full grid to record 'missing' species as absences
+BD_pilot_sched_pa <- full_grid %>% 
+  left_join(BD_pilot_sched_pa, by = c("site", "audiomoth_ID", "common_n", "scientific_n")) %>% 
+  mutate(
+    five_mins = replace_na(five_mins, 0),
+    ten_mins = replace_na(ten_mins, 0),
+    fithtn_mins = replace_na(fithtn_mins, 0),
+    thirty_mins = replace_na(thirty_mins, 0),
+    full_hour = replace_na(full_hour, 0)
+  )
+
+# check dataframe
+View(BD_pilot_sched_pa)
+
+
+
+##### No. Detections Summary #####
+
+# summarise detection counts
+BD_pilot_sched_ab <- BD_pilot_sched %>% 
+  group_by(site, audiomoth_ID, common_n, scientific_n) %>% 
+  summarise(
+    five_mins = sum(five_mins),
+    ten_mins = sum(ten_mins),
+    fithtn_mins = sum(fithtn_mins),
+    thirty_mins = sum(thirty_mins),
+    full_hour = sum(full_hour),
+    .groups = "drop"
+  )
+
+# merge with the full grid to record 'missing' species as absences
+BD_pilot_sched_ab <- full_grid %>% 
+  left_join(BD_pilot_sched_ab, by = c("site", "audiomoth_ID", "common_n", "scientific_n")) %>% 
+  mutate(
+    five_mins = replace_na(five_mins, 0),
+    ten_mins = replace_na(ten_mins, 0),
+    fithtn_mins = replace_na(fithtn_mins, 0),
+    thirty_mins = replace_na(thirty_mins, 0),
+    full_hour = replace_na(full_hour, 0)
+  )
+
+# check dataframe
+View(BD_pilot_sched_ab)
