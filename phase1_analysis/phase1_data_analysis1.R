469681b3f176a0a8c8c8d47c553c15d20b00496e
c-noonan-02 <charlottenoonan@outlook.com>
2025-06-10 16:57:07 +0100

    Added all paired devices combinations for distance analysis.

--- phase1_analysis/phase1_data_analysis1.R
+++ phase1_analysis/phase1_data_analysis1.R
@@ -1,3 +1,8 @@
+# This R script contains all code relating to sub-sets of the phase one data set
+# used to assess the differences in the number of species detected when using
+# different 'sampling effort' on different spatial and temporal scales, in
+# isolation from each other.
+
 # clear environment
 rm(list=ls())
 
@@ -26,19 +31,21 @@ BD_pilot_dist <- BD_pilot_data
 combo_200 <- tibble::tibble(
   
   # specify sites
-  site = c("BDWD", "BDWD", "BDWD", "BDWD", # woodland
+  site = c("BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
            
            "BDMD", "BDMD", "BDMD", "BDMD"), # moorland
   
   # specify their devices
-  audiomoth_ID = c("Audiomoth_9", "Audiomoth_3", "Audiomoth_4", "Audiomoth_18", # woodland
+  audiomoth_ID = c("Audiomoth_2", "Audiomoth_14", "Audiomoth_9", "Audiomoth_3", # woodland
+                   "Audiomoth_15", "Audiomoth_16", "Audiomoth_4", "Audiomoth_18", # woodland
                    
                      "Audiomoth_16", "Audiomoth_2", "Audiomoth_5", "Audiomoth_3"), # moorland
   
   # pair audiomoths that are 200m apart
   dist_200 = c("200_pair1", "200_pair1", "200_pair2", "200_pair2", # woodland
+               "200_pair3", "200_pair3", "200_pair4", "200_pair4", # woodland
                
-              "200_pair3", "200_pair3", "200_pair4", "200_pair4")) # moorland
+              "200_pair5", "200_pair5", "200_pair6", "200_pair6")) # moorland
 
 # add logical column in main dataframe to highlight rows which would be obtained with 200m spacing
 BD_pilot_dist <- BD_pilot_dist %>% 
@@ -57,7 +64,10 @@ combo_150 <- tibble::tibble(
   # specify sites
   site = c("BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
            "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
+           "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
+           "BDWD", "BDWD", # woodland
            
+           "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", # moorland
            "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD"), # moorland
   
   # specify their devices
@@ -65,18 +75,29 @@ combo_150 <- tibble::tibble(
                    "Audiomoth_15", "Audiomoth_20", "Audiomoth_4", "Audiomoth_5", # woodland
                    "Audiomoth_13", "Audiomoth_14", "Audiomoth_12", "Audiomoth_3", # woodland
                    "Audiomoth_1", "Audiomoth_16", "Audiomoth_7", "Audiomoth_18", # woodland
+                   "Audiomoth_2", "Audiomoth_4", "Audiomoth_13", "Audiomoth_7", # woodland
+                   "Audiomoth_6", "Audiomoth_19", "Audiomoth_10", "Audiomoth_5", # woodland
+                   "Audiomoth_14", "Audiomoth_18", # woodland
                    
                    "Audiomoth_12", "Audiomoth_7", "Audiomoth_16", "Audiomoth_14", # moorland
-                   "Audiomoth_4", "Audiomoth_15", "Audiomoth_6", "Audiomoth_2"), # moorland
+                   "Audiomoth_4", "Audiomoth_15", "Audiomoth_6", "Audiomoth_2", # moorland 
+                   "Audiomoth_12", "Audiomoth_5", "Audiomoth_11", "Audiomoth_20", # moorland
+                   "Audiomoth_13", "Audiomoth_17", "Audiomoth_6", "Audiomoth_2"), # moorland
+  
   
   # pair audiomoths that are 200m apart
   dist_150 = c("150_pair1", "150_pair1", "150_pair2", "150_pair2", # woodland
                "150_pair3", "150_pair3", "150_pair4", "150_pair4", # woodland
                "150_pair5", "150_pair5", "150_pair6", "150_pair6", # woodland
                "150_pair7", "150_pair7", "150_pair8", "150_pair8", # woodland
+               "150_pair9", "150_pair9", "150_pair10", "150_pair10", # woodland
+               "150_pair11", "150_pair11", "150_pair12", "150_pair12", # woodland
+               "150_pair13", "150_pair13", # woodland
                
-               "150_pair9", "150_pair9", "150_pair10", "150_pair10", # moorland
-               "150_pair11", "150_pair11", "150_pair12", "150_pair12")) # moorland
+               "150_pair14", "150_pair14", "150_pair15", "150_pair15", # moorland
+               "150_pair16", "150_pair16", "150_pair17", "150_pair17", # moorland
+               "150_pair18", "150_pair18", "150_pair19", "150_pair19", # moorland
+               "150_pair20", "150_pair20", "150_pair21", "150_pair21")) # moorland
 
 # add logical column in main dataframe to highlight rows which would be obtained with 200m spacing
 BD_pilot_dist <- BD_pilot_dist %>% 
@@ -96,7 +117,12 @@ combo_100 <- tibble::tibble(
   site = c("BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
            "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
            "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
+           "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
+           "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
+           "BDWD", "BDWD", "BDWD", "BDWD", # woodland
            
+           "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", # moorland
+           "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", # moorland
            "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", # moorland
            "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", # moorland
            "BDMD", "BDMD"), # moorland
@@ -108,12 +134,22 @@ combo_100 <- tibble::tibble(
                    "Audiomoth_1", "Audiomoth_20", "Audiomoth_7", "Audiomoth_5", # woodland
                    "Audiomoth_6", "Audiomoth_14", "Audiomoth_11", "Audiomoth_3", # woodland
                    "Audiomoth_17", "Audiomoth_16", "Audiomoth_19", "Audiomoth_18", # woodland
+                   "Audiomoth_2", "Audiomoth_15", "Audiomoth_9", "Audiomoth_4", # woodland
+                   "Audiomoth_13", "Audiomoth_1", "Audiomoth_12", "Audiomoth_7", # woodland
+                   "Audiomoth_6", "Audiomoth_17", "Audiomoth_11", "Audiomoth_19", # woodland
+                   "Audiomoth_10", "Audiomoth_20", "Audiomoth_8", "Audiomoth_5", # woodland
+                   "Audiomoth_14", "Audiomoth_16", "Audiomoth_3", "Audiomoth_18", # woodland
+                   
                    
                    "Audiomoth_12", "Audiomoth_13", "Audiomoth_16", "Audiomoth_18", # moorland
                    "Audiomoth_4", "Audiomoth_10", "Audiomoth_5", "Audiomoth_17", # moorland
                    "Audiomoth_11", "Audiomoth_7", "Audiomoth_6", "Audiomoth_14", # moorland
                    "Audiomoth_1", "Audiomoth_15", "Audiomoth_18", "Audiomoth_2", # moorland
-                   "Audiomoth_17", "Audiomoth_3"), # moorland
+                   "Audiomoth_17", "Audiomoth_3","Audiomoth_12", "Audiomoth_4", # moorland
+                   "Audiomoth_16", "Audiomoth_5", "Audiomoth_11", "Audiomoth_1", # moorland
+                   "Audiomoth_6", "Audiomoth_20", "Audiomoth_13", "Audiomoth_10", # moorland
+                   "Audiomoth_18", "Audiomoth_17", "Audiomoth_7", "Audiomoth_15", # moorland
+                   "Audiomoth_2", "Audiomoth_3"), # moorland
 
   # pair audiomoths that are 200m apart
   dist_100 = c("100_pair1", "100_pair1", "100_pair2", "100_pair2", # woodland
@@ -122,12 +158,21 @@ combo_100 <- tibble::tibble(
                "100_pair7", "100_pair7", "100_pair8", "100_pair8", # woodland
                "100_pair9", "100_pair9", "100_pair10", "100_pair10", # woodland
                "100_pair11", "100_pair11", "100_pair12", "100_pair12", # woodland
+               "100_pair13", "100_pair13", "100_pair14", "100_pair14", # woodland
+               "100_pair15", "100_pair15", "100_pair16", "100_pair16", # woodland
+               "100_pair17", "100_pair17", "100_pair18", "100_pair18", # woodland
+               "100_pair19", "100_pair19", "100_pair20", "100_pair20", # woodland
+               "100_pair21", "100_pair21", "100_pair22", "100_pair22", # woodland
                
-               "100_pair13", "100_pair13", "100_pair14", "100_pair14", # moorland
-               "100_pair15", "100_pair15", "100_pair16", "100_pair16", # moorland
-               "100_pair17", "100_pair17", "100_pair18", "100_pair18", # moorland
-               "100_pair19", "100_pair19", "100_pair20", "100_pair20", # moorland
-               "100_pair21", "100_pair21")) # moorland
+               "100_pair23", "100_pair23", "100_pair24", "100_pair24", # moorland
+               "100_pair25", "100_pair25", "100_pair26", "100_pair26", # moorland
+               "100_pair27", "100_pair27", "100_pair28", "100_pair28", # moorland
+               "100_pair29", "100_pair29", "100_pair30", "100_pair30", # moorland
+               "100_pair31", "100_pair31", "100_pair32", "100_pair32", # moorland
+               "100_pair33", "100_pair33", "100_pair34", "100_pair34", # moorland
+               "100_pair35", "100_pair35", "100_pair36", "100_pair36", # moorland
+               "100_pair37", "100_pair37", "100_pair38", "100_pair38", # moorland
+               "100_pair39", "100_pair39")) # moorland
 
 # add logical column in main dataframe to highlight rows which would be obtained with 100m spacing
 # duplicate rows for any device in multiple pairs
@@ -149,10 +194,17 @@ combo_50 <- tibble::tibble(
            "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
            "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
            "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
+           "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
+           "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
+           "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
+           "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", "BDWD", # woodland
            
            "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", # moorland
            "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", # moorland
-           "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD"), # moorland
+           "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", # moorland
+           "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", # moorland
+           "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", # moorland
+           "BDMD", "BDMD", "BDMD", "BDMD", "BDMD", "BDMD"), # moorland
   
   # specify their devices
   audiomoth_ID = c("Audiomoth_2", "Audiomoth_6", "Audiomoth_9", "Audiomoth_11", # woodland
@@ -163,6 +215,14 @@ combo_50 <- tibble::tibble(
                    "Audiomoth_17", "Audiomoth_20", "Audiomoth_19", "Audiomoth_5", # woodland
                    "Audiomoth_10", "Audiomoth_14", "Audiomoth_8", "Audiomoth_3", # woodland
                    "Audiomoth_20", "Audiomoth_16", "Audiomoth_5", "Audiomoth_18", # woodland
+                   "Audiomoth_2", "Audiomoth_9", "Audiomoth_9", "Audiomoth_15", # woodland
+                   "Audiomoth_15", "Audiomoth_4", "Audiomoth_13", "Audiomoth_12", # woodland
+                   "Audiomoth_12", "Audiomoth_1", "Audiomoth_1", "Audiomoth_7", # woodland
+                   "Audiomoth_6", "Audiomoth_11", "Audiomoth_11", "Audiomoth_17", # woodland
+                   "Audiomoth_17", "Audiomoth_19", "Audiomoth_10", "Audiomoth_8", # woodland
+                   "Audiomoth_8", "Audiomoth_20", "Audiomoth_20", "Audiomoth_5", # woodland
+                   "Audiomoth_14", "Audiomoth_3", "Audiomoth_3", "Audiomoth_16", # woodland
+                   "Audiomoth_16", "Audiomoth_18", # woodland
                    
                    
                    "Audiomoth_12", "Audiomoth_11", "Audiomoth_16", "Audiomoth_6", # moorland
@@ -170,7 +230,16 @@ combo_50 <- tibble::tibble(
                    "Audiomoth_11", "Audiomoth_13", "Audiomoth_6", "Audiomoth_18", # moorland
                    "Audiomoth_1", "Audiomoth_10", "Audiomoth_20", "Audiomoth_17", # moorland
                    "Audiomoth_13", "Audiomoth_7", "Audiomoth_18", "Audiomoth_14", # moorland
-                   "Audiomoth_10", "Audiomoth_15", "Audiomoth_14", "Audiomoth_2"), # moorland
+                   "Audiomoth_10", "Audiomoth_15", "Audiomoth_14", "Audiomoth_2", # moorland
+                   "Audiomoth_12", "Audiomoth_16", "Audiomoth_16", "Audiomoth_4", # moorland
+                   "Audiomoth_4", "Audiomoth_5", "Audiomoth_11", "Audiomoth_6", # moorland
+                   "Audiomoth_6", "Audiomoth_1", "Audiomoth_1", "Audiomoth_20", # moorland
+                   "Audiomoth_13", "Audiomoth_18", "Audiomoth_18", "Audiomoth_10", # moorland
+                   "Audiomoth_10", "Audiomoth_17", "Audiomoth_7", "Audiomoth_14", # moorland
+                   "Audiomoth_14", "Audiomoth_15"), # moorland
+                   
+                   
+                   
   
   # pair audiomoths that are 200m apart
   dist_50 = c("50_pair1", "50_pair1", "50_pair2", "50_pair2", # woodland
@@ -181,13 +250,27 @@ combo_50 <- tibble::tibble(
               "50_pair11", "50_pair11", "50_pair12", "50_pair12", # woodland
               "50_pair13", "50_pair13", "50_pair14", "50_pair14", # woodland 
               "50_pair15", "50_pair15", "50_pair16", "50_pair16", # woodland
-              
-              "50_pair17", "50_pair17", "50_pair18", "50_pair18", # moorland
-              "50_pair19", "50_pair19", "50_pair20", "50_pair20", # moorland
-              "50_pair21", "50_pair21", "50_pair22", "50_pair22", # moorland
-              "50_pair23", "50_pair23", "50_pair24", "50_pair24", # moorland
-              "50_pair25", "50_pair25", "50_pair26", "50_pair26", # moorland
-              "50_pair27", "50_pair27", "50_pair28", "50_pair28")) # moorland
+              "50_pair17", "50_pair17", "50_pair18", "50_pair18", # woodland
+              "50_pair19", "50_pair19", "50_pair20", "50_pair20", # woodland
+              "50_pair21", "50_pair21", "50_pair22", "50_pair22", # woodland
+              "50_pair23", "50_pair23", "50_pair24", "50_pair24", # woodland
+              "50_pair25", "50_pair25", "50_pair26", "50_pair26", # woodland
+              "50_pair27", "50_pair27", "50_pair28", "50_pair28", # woodland
+              "50_pair29", "50_pair29", "50_pair30", "50_pair30", # woodland
+              "50_pair31", "50_pair31", # woodland
+            
+              "50_pair32", "50_pair32", "50_pair33", "50_pair33", # moorland
+              "50_pair34", "50_pair34", "50_pair35", "50_pair35", # moorland
+              "50_pair36", "50_pair36","50_pair37", "50_pair37", # moorland
+              "50_pair38", "50_pair38", "50_pair39", "50_pair39", # moorland
+              "50_pair40", "50_pair40", "50_pair41", "50_pair41", # moorland
+              "50_pair42", "50_pair42","50_pair43", "50_pair43", # moorland
+              "50_pair44", "50_pair44", "50_pair45", "50_pair45", # moorland
+              "50_pair46", "50_pair46", "50_pair47", "50_pair47", # moorland
+              "50_pair48", "50_pair48", "50_pair49", "50_pair49", # moorland
+              "50_pair50", "50_pair50","50_pair51", "50_pair51", # moorland
+              "50_pair52", "50_pair52","50_pair53", "50_pair53", # moorland
+              "50_pair54", "50_pair54")) # moorland
 
 # add logical column in main dataframe to highlight rows which would be obtained with 100m spacing
 # duplicate rows for any device in multiple pairs
